597905fb1ba1fb934214057da5b5e468d1c823ad
ardonash <ardonash@stanford.edu>
2024-04-13 15:26:08 -0700

    tried again

--- test/test.py
+++ test/test.py
@@ -17,6 +17,7 @@ async def test_project(dut):
   dut._log.info("Reset")
   dut.ena.value = 1
   dut.ui_in.value = 0
+  dut.uio_oe.value = 0
   dut.uio_in.value = 0
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
